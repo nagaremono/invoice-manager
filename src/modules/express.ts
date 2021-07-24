add48a6abd4c60236b771c933bfaa28214a6eaf5
guruhedipurwanto <guruhedipurwanto@gmail.com>
2021-07-25 02:57:37 +0800

    add json parser middleware

--- src/modules/express.ts
+++ src/modules/express.ts
@@ -4,6 +4,8 @@ import { requestLogger } from '../middlewares/requestLogger';
 export const app = express();
 
 app.use(requestLogger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (_, res) => {
   res.send('hello world!');
